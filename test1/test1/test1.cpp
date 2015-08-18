cbeb6ea063f94e05ecccbb9af8a035e7d49b044e
ehphan <mrmuko@gmail.com>
2015-08-17 17:18:26 -0700

    initial commit of test

---
+++ test1/test1/test1.cpp
@@ -0,0 +1,17 @@
+// test1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "HI";
+
+	cin.get();
+	return 0;
+}
+
